4e66606b570cd5ff7f71e04655d013cb86711762
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-08-31 15:13:40 -0300

    floyd-triangle

--- loops/provas_loops/floyd_triangle.c
+++ loops/provas_loops/floyd_triangle.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!");
+int main()
+{
+    int inputTriangle = 0;
+    int contI = 1;
+    int contJ = 1;
+    int contG = 1;
+    scanf("%d", &inputTriangle);
+    if (inputTriangle <= 0)
+    {
+        printf("Você entrou com %d, tente de novo na próxima", inputTriangle);
+    }
+    else
+    {
+        for (int j = 0; j < inputTriangle; j++)
+        {
+
+            for (int i = 0; i < contI; i++)
+            {
+                if (contG < 10)
+                {
+                    printf(" %d ", contG++);
+                }
+                else
+                {
+                    printf("%d ", contG++);
+                }
+            }
+            contI++;
+            printf("\n");
+        }
+    }
 }
