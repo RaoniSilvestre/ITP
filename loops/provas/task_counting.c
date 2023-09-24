d40ef31458df1c2d8fc3fdd147b7c67e81b260d1
Raoni Silva <raoni.silvestre@hotmail.com>
2023-09-24 19:54:41 -0300

    ?

---
+++ loops/provas/task_counting.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int qntClubes = 0;
+    char esporteAtleta = 'A';
+    int salto = 0;
+    int corrida = 0;
+    int lancamento = 0;
+    int sumAtletas = 0;
+    scanf("%d", &qntClubes);
+    for (int i = 0; i < qntClubes; i++ )
+    {
+        while (esporteAtleta != 'F')
+        {
+            scanf("%c", &esporteAtleta);
+            if (esporteAtleta == 'S')
+            {
+                salto += 1;
+            }
+            else if (esporteAtleta == 'C')
+            {
+                corrida += 1;
+            }
+            else if (esporteAtleta == 'L')
+            {
+                lancamento += 1;
+            }
+        }
+        esporteAtleta = 'A';
+    }
+    sumAtletas = corrida + salto + lancamento;
+    printf("%d %d %d %d", corrida, salto, lancamento,sumAtletas);
+}
