d3f0eddc5697b1831810e425d4ab03b2c2a434d9
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-04 12:19:51 -0300

    euclides eu te amo

---
+++ 10-recursão/provas/mdc&mmc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int calcMDC(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  return calcMDC(b, a % b);
+}
+
+int main(int argc, char *argv[]) {
+  int a, b;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  int mdc = calcMDC(a, b);
+  int mmc = (a*b)/mdc;
+  printf("MDC = %d, MMC = %d", mdc, mmc);
+  return 0;
+}
